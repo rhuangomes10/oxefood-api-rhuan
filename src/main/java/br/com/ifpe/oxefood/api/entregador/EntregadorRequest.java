a7609aa11f2e8f3fafc9d18816ed28fa601ed082
rhuangomes10 <rhuancraft189gg@gmail.com>
2026-02-25 16:34:52 -0300

    Cliente, Produto e Entregador concluídos

---
+++ src/main/java/br/com/ifpe/oxefood/api/entregador/EntregadorRequest.java
@@ -0,0 +1,69 @@
+package br.com.ifpe.oxefood.api.entregador;
+
+import java.time.LocalDate;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import br.com.ifpe.oxefood.modelo.entregador.Entregador;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class EntregadorRequest {
+
+    private String nome;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
+    private LocalDate dataNascimento;
+
+    private String cpf;
+
+    private String foneCelular;
+
+    private String foneFixo;
+
+    private Integer qtdEntregasRealizadas;
+
+    private Double valorFrete;
+
+    private String enderecoRua;
+
+    private String enderecoComplemento;
+
+    private String enderecoNumero;
+
+    private String enderecoBairro;
+
+    private String enderecoCidade;
+
+    private String enderecoCep;
+
+    private String enderecoUf;
+
+    private Boolean ativo;
+
+    public Entregador build() {
+        return Entregador.builder()
+                .nome(nome)
+                .dataNascimento(dataNascimento)
+                .cpf(cpf)
+                .foneCelular(foneCelular)
+                .foneFixo(foneFixo)
+                .qtdEntregasRealizadas(qtdEntregasRealizadas)
+                .valorFrete(valorFrete)
+                .enderecoRua(enderecoRua)
+                .enderecoComplemento(enderecoComplemento)
+                .enderecoNumero(enderecoNumero)
+                .enderecoBairro(enderecoBairro)
+                .enderecoCidade(enderecoCidade)
+                .enderecoCep(enderecoCep)
+                .enderecoUf(enderecoUf)
+                .ativo(ativo)
+                .build();
+    }
+}
