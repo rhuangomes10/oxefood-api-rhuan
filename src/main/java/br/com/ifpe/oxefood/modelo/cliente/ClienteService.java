b8ffb01afec40f4aaa2a2cc79d5991ee8a853e45
Aluno <aluno@ifpe>
2026-03-11 16:14:55 -0300

    Remoção adicionada

--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -1,4 +1,5 @@
 package br.com.ifpe.oxefood.modelo.cliente;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,7 +9,7 @@ import jakarta.transaction.Transactional;
 
 @Service
 public class ClienteService {
-        
+
     @Autowired
     private ClienteRepository repository;
 
@@ -18,8 +19,9 @@ public class ClienteService {
         cliente.setHabilitado(Boolean.TRUE);
         return repository.save(cliente);
     }
+
     public List<Cliente> listarTodos() {
-  
+
         return repository.findAll();
     }
 
@@ -29,18 +31,26 @@ public class ClienteService {
     }
 
     @Transactional
-   public void update(Long id, Cliente clienteAlterado) {
+    public void update(Long id, Cliente clienteAlterado) {
 
-      Cliente cliente = repository.findById(id).get();
-      cliente.setNome(clienteAlterado.getNome());
-      cliente.setDataNascimento(clienteAlterado.getDataNascimento());
-      cliente.setCpf(clienteAlterado.getCpf());
-      cliente.setFoneCelular(clienteAlterado.getFoneCelular());
-      cliente.setFoneFixo(clienteAlterado.getFoneFixo());
-	    
-      repository.save(cliente);
-  }
+        Cliente cliente = repository.findById(id).get();
+        cliente.setNome(clienteAlterado.getNome());
+        cliente.setDataNascimento(clienteAlterado.getDataNascimento());
+        cliente.setCpf(clienteAlterado.getCpf());
+        cliente.setFoneCelular(clienteAlterado.getFoneCelular());
+        cliente.setFoneFixo(clienteAlterado.getFoneFixo());
 
+        repository.save(cliente);
+    }
+
+    @Transactional
+   public void delete(Long id) {
+
+       Cliente cliente = repository.findById(id).get();
+       cliente.setHabilitado(Boolean.FALSE);
+
+       repository.save(cliente);
+   }
 
 
 }
