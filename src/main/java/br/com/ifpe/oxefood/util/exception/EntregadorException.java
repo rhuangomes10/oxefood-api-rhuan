f5c9cc3ade56d28e9f143e08816364603c4ae4c6
Aluno <aluno@ifpe>
2026-04-15 16:51:22 -0300

    Expection de Entregador

---
+++ src/main/java/br/com/ifpe/oxefood/util/exception/EntregadorException.java
@@ -0,0 +1,15 @@
+package br.com.ifpe.oxefood.util.exception;
+
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.http.HttpStatus;
+
+@ResponseStatus(code = HttpStatus.INTERNAL_SERVER_ERROR)
+
+public class EntregadorException extends RuntimeException {
+    
+    public EntregadorException(String msg) {
+
+	super(String.format(msg));
+    }
+
+}
