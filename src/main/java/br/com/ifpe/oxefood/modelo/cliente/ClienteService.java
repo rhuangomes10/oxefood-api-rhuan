a74458323861a3a11727f6b76ea60691547418bd
aluno@ifpe <aluno@ifpe>
2026-04-29 16:12:11 -0300

    Controle de acesso Adicionado

--- src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
+++ src/main/java/br/com/ifpe/oxefood/modelo/cliente/ClienteService.java
@@ -6,6 +6,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import br.com.ifpe.oxefood.modelo.acesso.Perfil;
+import br.com.ifpe.oxefood.modelo.acesso.PerfilRepository;
+import br.com.ifpe.oxefood.modelo.acesso.UsuarioService;
 import br.com.ifpe.oxefood.util.exception.ClienteException;
 import jakarta.transaction.Transactional;
 
@@ -18,8 +21,23 @@ public class ClienteService {
     @Autowired
     private EnderecoClienteRepository enderecoClienteRepository;
 
+    @Autowired
+   private UsuarioService usuarioService;
+
+   @Autowired
+   private PerfilRepository perfilUsuarioRepository;
+
+
     @Transactional
     public Cliente save(Cliente cliente) throws ClienteException {
+
+        usuarioService.save(cliente.getUsuario());
+
+      for (Perfil perfil : cliente.getUsuario().getRoles()) {
+           perfil.setHabilitado(Boolean.TRUE);
+           perfilUsuarioRepository.save(perfil);
+      }
+
 
         if (cliente.getFoneCelular().startsWith("81") == false) {
             throw new ClienteException(ClienteException.MSG_TELEFONE_SEM_PREFIXO);
